c6b0d90d83ba47ead6ca3eb64656b7471b621bf1
lixuejian <smilelixuejian@gmail.com>
2019-12-16 14:22:39 +0800

    解答第27题

--- leetcode_C++/27.移除元素.cpp
+++ leetcode_C++/27.移除元素.cpp
@@ -8,7 +8,16 @@
 class Solution {
 public:
     int removeElement(vector<int>& nums, int val) {
-        
+        int pos = 0;
+        for (int i = 0; i < nums.size(); i++)
+        {
+            if (nums[i] != val)
+            {
+                nums[pos] = nums[i];
+                pos++;
+            }
+        }
+        return pos;
     }
 };
 // @lc code=end
