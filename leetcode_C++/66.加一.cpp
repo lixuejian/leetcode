c6b7795b4906141f50b2b46d7c1edd72026f095d
lixuejian <smilelixuejian@gmail.com>
2019-12-19 14:02:57 +0800

    解答66题

--- leetcode_C++/66.加一.cpp
+++ leetcode_C++/66.加一.cpp
@@ -8,6 +8,24 @@
 class Solution {
 public:
     vector<int> plusOne(vector<int>& digits) {
+        int len = digits.size() - 1;
+        for (int i = len; i >= 0 ; i--)
+        {
+            if (digits[i] == 9)
+            {
+                digits[i] = 0;
+            }else
+            {
+                digits[i] +=1;
+                break;
+            }
+        }
+        if (digits[0] == 0)
+        {
+            digits.push_back(0);
+            digits[0] = 1;            
+        }
+        return digits;
         
     }
 };
