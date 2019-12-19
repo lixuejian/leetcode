c040e23f4eace16c90417a3da6ae5aa693f7563a
lixuejian <smilelixuejian@gmail.com>
2019-12-19 13:40:51 +0800

    解答53题

--- leetcode_C++/53.最大子序和.cpp
+++ leetcode_C++/53.最大子序和.cpp
@@ -8,7 +8,23 @@
 class Solution {
 public:
     int maxSubArray(vector<int>& nums) {
-        
+        int max = INT_MIN;
+        int numsize = int(nums.size());
+        for (int i = 0; i < numsize; i++)
+        {
+            int sum = 0;
+            for (int j = i; j < numsize; j++)
+            {
+                sum = sum + nums[j];
+                if (sum > max)
+                {
+                    max = sum;
+                }
+                
+            }
+            
+        }
+        return max; 
     }
 };
 // @lc code=end
