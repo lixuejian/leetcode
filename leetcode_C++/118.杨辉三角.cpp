c1fbc1a4fbe394ed87d154cc0fea5ffea495ecd9
lixuejian <smilelixuejian@gmail.com>
2019-12-25 09:08:31 +0800

    解答118提

--- leetcode_C++/118.杨辉三角.cpp
+++ leetcode_C++/118.杨辉三角.cpp
@@ -8,7 +8,19 @@
 class Solution {
 public:
     vector<vector<int>> generate(int numRows) {
-        
+        vector<vector<int>> ans(numRows);
+        if(numRows == 0)    return ans; //若numRows为空，返回空数组
+        for(int i = 0; i < numRows; ++ i ) //给数组一个个赋值
+        {
+            for(int j = 0; j <= i; ++ j)
+            {
+                if(j == 0 || j == i) //若是左右两边的边界，赋值为1
+                    ans[i].push_back(1);
+                else
+                    ans[i].push_back(ans[i-1][j-1] + ans[i-1][j]); //否则赋值为该位置左上与右上的和
+            }
+        }
+        return ans;
     }
 };
 // @lc code=end
